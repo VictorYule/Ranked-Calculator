cefdce1e0d3eb2bda8589bd3ca81a0e05d056ee4
Victor Yule <105625896+VictorYule@users.noreply.github.com>
2023-12-19 20:52:55 -0300

    alteracao de codigo

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Crie uma função que recebew como parâmetro a quantidade de vitórias e derrotas de um jogador,
+//depois disso retorne o resultado para uma variável, o salvo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+//Se vitórias for menor que 10 = Ferro
+//Se vitórias for entre 11 e 20 = Bronze
+//Se vitórias for entre 21 e 50 = Prata
+//Se vitórias for entre 51 e 80 = Ouro
+//Se vitórias for entre 81 e 90 = Diamante
+//Se vitórias for entre 91 e 100 = Lendário
+//Se vitórias for maior ou igual a 101 = Imortal
+
 function calcula_nivel(vitorias, derrotas) {
     let saldo = vitorias - derrotas;
     let niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"];
